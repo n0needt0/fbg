b1a3645d909823e6f87a4d72c82f069319472dc0
n0needt0 <andrew@yasinsky.com>
2014-06-23 16:33:09 -0700

    api node

--- appliance/z_api/api-bootstrap.sh
+++ appliance/z_api/api-bootstrap.sh
@@ -5,7 +5,18 @@ apt-get update
 apt-get install python-software-properties
 add-apt-repository ppa:semiosis/ubuntu-glusterfs-3.4
 apt-get update
-apt-get install glusterfs-client -y
+apt-get install glusterfs-client ganglia-monitor nagios-nrpe-server curl -y
+
+sudo apt-get install git -y
+sudo apt-get install mercurial -y
+sudo apt-get install make -y
+sudo apt-get install binutils -y
+sudo apt-get install bison -y
+sudo apt-get install gcc -y
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get -q -y install mysql-server
+apt-get install php5-mysql nginx php5-fpm -y
 
 echo "10.10.10.11		es1" >> /etc/hosts
 echo "10.10.10.12		es2" >> /etc/hosts
@@ -16,8 +27,13 @@ echo "10.10.10.23		gfs3" >> /etc/hosts
 echo "10.10.10.31		api" >> /etc/hosts
 echo "10.10.10.41		monitor" >> /etc/hosts
 
-mkdir /storage-pool
+mkdir /gfs
 
-sudo mount -t glusterfs gfs1:/gluster-volume /storage-pool
+sudo mount -t glusterfs gfs1:/gluster-volume /gfs
 
-chmod 777 /storage-pool
+chmod 777 /gfs
+
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
+gvm install go1
+gvm use go1 
