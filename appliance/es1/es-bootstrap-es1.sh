83bbb9aed6250fead28b0894d65f0cde3a54de9a
n0needt0 <andrew@yasinsky.com>
2014-06-23 09:14:12 -0700

    elastic tweaks

--- appliance/es1/es-bootstrap-es1.sh
+++ appliance/es1/es-bootstrap-es1.sh
@@ -4,7 +4,7 @@
 sudo su
 
 sudo apt-get update
-sudo apt-get install openjdk-7-jre-headless ganglia-monitor -y
+sudo apt-get install openjdk-7-jre-headless ganglia-monitor nagios-nrpe-server -y
 
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.deb
 sudo dpkg -i elasticsearch-1.2.1.deb
@@ -25,7 +25,7 @@ sed -i 's/#cluster.name: elasticsearch/cluster.name: fileroom/g' /etc/elasticsea
 sed -i 's/#node.name: "Franz Kafka"/node.name: "es1"/g' /etc/elasticsearch/elasticsearch.yml 
 sed -i 's/#network.host: 192.168.0.1/network.host: 10.10.10.11/g' /etc/elasticsearch/elasticsearch.yml 
 sed -i 's/#transport.tcp.compress: true/transport.tcp.compress: true/g' /etc/elasticsearch/elasticsearch.yml 
-sed -i 's/#index.number_of_replicas: 1/#index.number_of_replicas: 3/g' /etc/elasticsearch/elasticsearch.yml 
+sed -i 's/#index.number_of_replicas: 1/index.number_of_replicas: 3/g' /etc/elasticsearch/elasticsearch.yml 
 sed -i 's/#discovery.zen.minimum_master_nodes: 1/discovery.zen.minimum_master_nodes: 2/g' /etc/elasticsearch/elasticsearch.yml
 
 #sed -i 's//g' /etc/elasticsearch/elasticsearch.yml 
@@ -39,3 +39,5 @@ bin/plugin -i elasticsearch/marvel/latest
 echo "marvel.agent.exporter.es.hosts: [\"10.10.10.11:9200\",\"10.10.10.12:9200\",\"10.10.10.13:9200\"]" >> /etc/elasticsearch/elasticsearch.yml
 
 /etc/init.d/elasticsearch restart
+
+#ADD NAGIOS AND GANGLIA CONFIGS
