ed43779f0917277dde7e699279e00f8660b38a55
n0needt0 <andrew@yasinsky.com>
2014-06-26 16:12:23 -0700

    -ngnx conf

--- appliance/z_api/api-bootstrap.sh
+++ appliance/z_api/api-bootstrap.sh
@@ -15,6 +15,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server mysql-client
 apt-get install nginx php5-fpm php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php-apc php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl -y
 
+
+rm /etc/nginx/sites-enabled/*
+rm /etc/nginx/sites-available/*
+
+cat /vagrant/api_nginx >> /etc/nginx/sites-enabled/api
+
 /etc/init.d/php5-fpm reload
 /etc/init.d/nginx reload
 
