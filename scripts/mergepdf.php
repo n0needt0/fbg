290bd1afdcb585b87cf726967ad262016d919d68
metal-oxide-semiconductor <bbarantschik@gmail.com>
2014-06-26 13:58:47 -0700

    fixed script for combining pdfs

--- scripts/mergepdf.php
+++ scripts/mergepdf.php
@@ -1,12 +1,14 @@
-//Requires GhostScript
+<?php>
+//Requires GhostScript	
 $fileArray= scandir("./test");
-
+print_r($fileArray);
 $datadir = "./";
 $outputName = $datadir."merged.pdf";
 
 $cmd = "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$outputName ";
 //Add each pdf file to the end of the command
-foreach($fileArray as $file) {
-    $cmd .= "./test".$file." ";
+foreach($fileArray as $file) if($file != "." && $file != "..") {
+    $cmd .= "./test/".$file." ";
 }
+print $cmd;
 $result = shell_exec($cmd);
