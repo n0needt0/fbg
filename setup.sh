73aa60bdcff6c9cadb36239ecb12eb291f2021cb
n0needt0 <andrew@yasinsky.com>
2014-07-08 16:50:29 -0700

    test2

--- setup.sh
+++ setup.sh
@@ -215,21 +215,4 @@ cp -r config /home/$username/fbg
 cp -r backend /home/$username/fbg
 chown -R $username:$username /home/$username
 
-#beam me up Scotty
-
-su $username
-
-cd /home/$username/fbg/appliance
-
-#turn on vagrants
-for D in *; do
-    if [[ -d "${D}" && -e "${D}/Vagrantfile" ]]; then
-        cd "${D}"
-        echo "Installing ${D} virtual machine..."
-        sleep 1
-        vagrant up
-        cd ..
-    fi
-done
-
-#/etc/init.d/vboxweb-service restart
+/etc/init.d/vboxweb-service restart
