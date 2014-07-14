c664d058daa274a3ddcbe40046c14df10abf2738
n0needt0 <andrew@yasinsky.com>
2014-07-14 14:56:20 -0700

    config/fbg.

--- backend/code/var/api/app/config/fbg.php
+++ backend/code/var/api/app/config/fbg.php
@@ -1,15 +1,8 @@
 <?php
 return array(
-                'levels' => array(
-                                '1'=>array('plan_name'=>'Free', 'ratlimit'=>5,'ttlmin'=>1,'urlto'=>false, 'emailto'=>false),
-                                '2'=>array('plan_name'=>'Paid', 'ratlimit'=>100,'ttlmin'=>1,'urlto'=>false, 'emailto'=>true),
-                                '3'=>array('plan_name'=>'Pro', 'ratlimit'=>500,'ttlmin'=>5,'urlto'=>true, 'emailto'=>true),
-                                '4'=>array('plan_name'=>'Corp', 'ratlimit'=>1000,'ttlmin'=>5,'urlto'=>true, 'emailto'=>true)
-                ),
-                'defaults' => array(
-                                'crontab'=>'0 0 * * *',
-                                'allow'=>86399,
-                                'min_allow'=>300
-                                )
+                'es'=>'localhost',
+                'pdfs'=>'/gfs',
+                //this is where file room defined
+                'fileroom'=>array()
 );
 
