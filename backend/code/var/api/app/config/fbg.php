141b0e85635c4da20ad82c03562ce6a661b3b14f
metal-oxide-semiconductor <bbarantschik@gmail.com>
2014-07-14 15:19:09 -0700

    added layout

--- backend/code/var/api/app/config/fbg.php
+++ backend/code/var/api/app/config/fbg.php
@@ -1,26 +1,18 @@
 <?php
 return array(
+    'defaults' => array(
+        'crontab'=>'0 0 * * *',
+        'allow'=>86399,
+        'min_allow'=>300
+    ),
+
     'levels' => array(
         '1'=>array('plan_name'=>'Free', 'ratlimit'=>5,'ttlmin'=>1,'urlto'=>false, 'emailto'=>false),
         '2'=>array('plan_name'=>'Paid', 'ratlimit'=>100,'ttlmin'=>1,'urlto'=>false, 'emailto'=>true),
         '3'=>array('plan_name'=>'Pro', 'ratlimit'=>500,'ttlmin'=>5,'urlto'=>true, 'emailto'=>true),
         '4'=>array('plan_name'=>'Corp', 'ratlimit'=>1000,'ttlmin'=>5,'urlto'=>true, 'emailto'=>true)
     ),
-    'defaults' => array(
-        'crontab'=>'0 0 * * *',
-        'allow'=>86399,
-        'min_allow'=>300
-    )'levels' => array(
-    '1'=>array('plan_name'=>'Free', 'ratlimit'=>5,'ttlmin'=>1,'urlto'=>false, 'emailto'=>false),
-    '2'=>array('plan_name'=>'Paid', 'ratlimit'=>100,'ttlmin'=>1,'urlto'=>false, 'emailto'=>true),
-    '3'=>array('plan_name'=>'Pro', 'ratlimit'=>500,'ttlmin'=>5,'urlto'=>true, 'emailto'=>true),
-    '4'=>array('plan_name'=>'Corp', 'ratlimit'=>1000,'ttlmin'=>5,'urlto'=>true, 'emailto'=>true)
-),
-                'defaults' => array(
-    'crontab'=>'0 0 * * *',
-    'allow'=>86399,
-    'min_allow'=>300
-)
+
     'fileroom' => array(
         'folder' => array(
             'example' => 'SHMOE_JOE_1234',
